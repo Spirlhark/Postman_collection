23be081120b1edbc4f65e8e8dcb6c57eea759390
Home <home@MacBook-Pro-Home.local>
2024-07-29 17:06:09 +0300

    test notif 11

--- results.py
+++ results.py
@@ -1,15 +1,27 @@
 import json
 import os
-from slack_sdk.webhook import WebhookClient
 import requests
 
-with open("./testResults/results.json") as file:
-# with open("./Postman_collection/testResults/results.json") as file:
-# with open("testResults/results.json") as file:
+url_slack = os.environ.get('SLACK_WEBHOOK_URL2')
+text1 = os.environ.get('GITHUB_REPOSITORY')
+text3 = os.environ['GITHUB_REF_NAME'] 
+
+
+# with open("./testResults/results.json") as file:
+with open("./allure-report/widgets/suites.json") as file:
     stock = json.load(file)
 
-total_tests = stock['run']['stats']['assertions']['total']
-failed_tests = stock['run']['stats']['assertions']['failed']
+print(stock)
+# item1 = stock['items'][0]
+# print(item1)
+
+
+# total_tests = (stock['run']['stats']['assertions']['total'])
+total_tests = 8
+
+# failed_tests = (stock['run']['stats']['assertions']['failed'])
+failed_tests = 6
+
 passed_tests = total_tests - failed_tests
 print(total_tests)
 print(failed_tests)
@@ -19,20 +31,14 @@ icon = ":white_check_mark:"
 
 if failed_tests > 0:
     icon = ":x:"
-text1 = os.environ.get('GITHUB_REPOSITORY')
-text2 = os.environ['GITHUB_REPOSITORY']
-text3 = os.environ['GITHUB_REF_NAME']
+# text1 = 'Postman_collection'
+# text3 = 'master'
 
 text = f"Postman collection results_[{text3}]: \n" \
        f" [{text1}] \n" \
        f" {icon} total: {total_tests}, passed: {passed_tests}, failed: {failed_tests}\n " \
        f"<https://spirlhark.github.io/Postman_collection/| Allure-report>"
 
-# print(text)
-# url = os.environ["SLACK_WEBHOOK_URL"]
-url_slack = os.environ.get('SLACK_WEBHOOK_URL2')
-print(url_slack)
-# webhook = WebhookClient(url)
 print(111)
 
 header = {'Content-type': 'application/json'}
@@ -47,7 +53,7 @@ data_body = {
         }]
 }
 
-response2 = requests.post(url_slack, json=data_body, headers=header)
+response = requests.post(url_slack, json=data_body, headers=header)
 
 print(222)
 
